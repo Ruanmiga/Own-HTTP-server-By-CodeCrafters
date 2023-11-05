3eeb0361ceb79b739b19894b42f21bf96e0f8ff9
Antonio <ruanmiga@Ruanmiga.localhost>
2023-11-05 15:15:31 -0600

    pass 7st stage

--- app/main.js
+++ app/main.js
@@ -19,7 +19,6 @@ const server = net.createServer((socket) => {
         socket.write(makeResponse("200 Ok", "text/plain", param.length, param));
         }
         else if(path.includes("files")){
-            console.log(directory);
         const fileName = path.substring(7, path.length);
         const filePath = join(directory, fileName);
 
