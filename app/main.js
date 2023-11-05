63d297770c63c741a0a031e3b59fd53ac59218e4
Antonio <ruanmiga@Ruanmiga.localhost>
2023-11-05 14:17:44 -0600

    pass 4st stage

--- app/main.js
+++ app/main.js
@@ -5,7 +5,16 @@ const server = net.createServer((socket) => {
         const path = (data.toString().split("\r\n")[0]).split(" ")[1];
         if(path === "/") {
         socket.write("HTTP/1.1 200 OK\r\n\r\n");
-        }else if(path.includes("echo")){
+        }else if(path === "/user-agent"){
+        const userAgent = (data.toString().split("\r\n")[4]).split(" ")[1];
+        console.log(userAgent);
+        
+        socket.write("HTTP/1.1 200 OK\r\n");
+        socket.write("Content-Type: text/plain\r\n");
+        socket.write("Content-Length: " + userAgent.length + "\r\n\r\n")
+        socket.write(userAgent);
+        }
+        else if(path.includes("echo")){
         const param = path.substring(6, path.length);
         console.log(param);
         socket.write("HTTP/1.1 200 OK\r\n");
