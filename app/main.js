c2af6c266c12a258c567c1a1dab86474127905b8
Antonio <ruanmiga@Ruanmiga.localhost>
2023-11-05 17:29:04 -0600

    pass 8st stage

--- app/main.js
+++ app/main.js
@@ -87,7 +87,7 @@ function searchHeader(name, data){
 function checkRoute(data, path, method){
     const fullPath = extractPath(data);
     const arr = fullPath.split("/");
-    const hasPath = fullPath.split("/").length === 2 ? fullPath === path : fullPath.startsWith(path);
+    const hasPath = fullPath.split("/").length != 2 ? fullPath.startsWith(path) : fullPath === path;
     console.log(fullPath, arr, path, hasPath, fullPath.startsWith(path));
 
     return hasPath && extractMethod(data) === method; 
