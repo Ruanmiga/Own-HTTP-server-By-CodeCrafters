a21b230d73b8712bd4cb80a236bd0580c1955185
Antonio <ruanmiga@Ruanmiga.localhost>
2023-11-05 13:59:08 -0600

    pass 2st stage

--- app/main.js
+++ app/main.js
@@ -2,8 +2,12 @@ const net = require("net");
 
 const server = net.createServer((socket) => {
     socket.on("data", (data) => {
-        console.log(data.toString().split("\r\n")[0]);
+        const path = (data.toString().split("\r\n")[0]).split(" ")[1];
+        if(path === "/") {
         socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        } else {
+        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
         socket.end();
     });
     socket.on("close", () => {
