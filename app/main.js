74ab9cb708f818a75c543651d3c0ee4fa01f3285
Antonio <ruanmiga@Ruanmiga.localhost>
2023-11-05 13:51:38 -0600

    pass 1st stage

--- app/main.js
+++ app/main.js
@@ -1,13 +1,9 @@
 const net = require("net");
 
 const server = net.createServer((socket) => {
-    socket.on("connect", () => {
+    socket.on("close", () => {
         socket.write("HTTP/1.1 200 OK\r\n\r\n");
         socket.end();
-    });
-
-    socket.on("close", () => {
-     socket.end();
      server.close();
  });
 });
